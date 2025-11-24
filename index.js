da7f37d5757430f1ea7c73cd03bec9318dc28ef6
Sergey <seregaa99@bk.ru>
2025-11-24 14:10:43 +0600

    первый трай после долгого перерыва

---
+++ index.js
@@ -0,0 +1,35 @@
+const firstNameEl = document.getElementById("first-name")
+const goButton = document.getElementById("make-changes")
+firstNameEl.value = "Andrey"
+const getNameFirstName = document.getElementById("first-name")
+
+goButton.onclick = function () {
+    alert(lastNameEl.value)
+}
+const lastNameEl = document.getElementById("last-name")
+lastNameEl.value = 'Hachapuri'
+
+var addressEl = document.getElementById("address")
+addressEl.value = 'Dom hinkal'
+
+var citiesEl = document.getElementById('cities')
+citiesEl.value = 'Minsk'
+
+var avatarEl = document.getElementById('avatar')
+avatarEl.src = "https://avatars.mds.yandex.net/i?id=2a0000019a797b61dd1fc4c6da9226b64007-1385197-fast-images&n=13"
+
+const div = document.createElement("div")
+
+div.innerHTML = '<ul id="technologies-list"><li>js sedsdsdssdsdsdsdsdsdsdsdsdsdsdsdsdsdsds</li><li>css sdsdsdsdsdsdsdsdsdsdsds</li></ul>'
+document.body.append(div)
+innerEl = document.getElementById('technologies-list')
+
+function alertValues() {
+    console.log('1');
+    console.log('2');
+    console.log('3');
+
+
+}
+alertValues();
+
